70075c00ee0d0948c01849a2402ffa7b23f4af25
Kellen Lask <kellenlask@gmail.com>
2016-07-04 23:02:43 -0600

    Updated git ignore

--- FractalTree.py
+++ FractalTree.py
@@ -1,12 +1,12 @@
-import pygame, math
+import pygame, math, pygame.gfxdraw
 
 # Some constants
 surface_height = 1000
 surface_width = 1600
-surface_background = (255, 255, 255)
-color = (0, 0, 255)
+surface_background = (53, 70, 92)
+color = (127, 138, 152)
 scale_ratio = 0.29
-default_theta = 1.047 # 60 degree rotations in radians
+default_theta = .30 # 60 degree rotations in radians
 default_recursion_depth = 15
 
 # Draws the fractal tree. order = drawings left, depth = drawings completed
@@ -23,7 +23,7 @@ def draw_tree( order, theta, size, position, orientation, depth = 0 ) :
 	end_position = (x + dx, y + dy)
 
 	# Draw the branch
-	pygame.draw.line( drawing_surface, color, position, end_position )
+	pygame.draw.line(drawing_surface, color, position, end_position)
 
 	# If we haven't reached the bottom yet...
 	if order > 0 :
@@ -44,7 +44,7 @@ def run( ) :
 		# Grab user input to quit
 		ev = pygame.event.poll( )
 		if ev.type == pygame.QUIT :
-			break;
+			break
 
 # Create the drawing surface
 drawing_surface = pygame.display.set_mode( (surface_width, surface_height) )
